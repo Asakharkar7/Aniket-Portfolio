6d78cbd62eae112bc39d9e1f5602d0d6000510a1
Aniket Sakharkar <aniketrita2612@gmail.com>
2025-11-15 16:27:52 -0500

    Updated Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -57,12 +57,13 @@ export default function Hero() {
               <span className="hidden sm:inline">aniketsakharkar4@gmail.com</span>
             </a>
             <span className="text-slate-400">•</span>
+            
             <a
-              href="tel:+1 857-230-5126"
+              href="tel:+18572305126"
               className="flex items-center gap-2 text-slate-600 hover:text-slate-900 transition-colors"
               >
               <Phone size={20} />
-              <span className="hidden sm:inline">+1857-230-5126/span>
+              <span className="hidden sm:inline">+18572305126/span>
             </a>
 
 
