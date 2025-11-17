16b0e47ec1dea7a3904df6fc1d5bc69bc31d67f7
Aniket Sakharkar <aniketrita2612@gmail.com>
2025-11-16 23:02:29 -0500

    Updated AvatarAnimation.tsx

---
+++ src/components/AvatarAnimation.tsx
@@ -0,0 +1,34 @@
+import Lottie from "react-lottie-player";
+import { motion } from "framer-motion";
+import deskAnimation from "../assets/desk.json"; // 👈 your downloaded file
+
+export default function AvatarAnimation() {
+  return (
+    <div className="relative flex flex-col items-center justify-center">
+      {/* Avatar at desk */}
+      <Lottie
+        loop
+        animationData={deskAnimation}
+        play
+        style={{ width: 300, height: 300 }}
+      />
+
+      {/* Floating charts */}
+      <motion.div
+        className="absolute top-10 left-10 bg-blue-500 w-16 h-16 rounded-lg"
+        animate={{ y: [0, -20, 0] }}
+        transition={{ repeat: Infinity, duration: 3 }}
+      />
+      <motion.div
+        className="absolute top-20 right-10 bg-green-500 w-20 h-12 rounded-lg"
+        animate={{ y: [0, -15, 0] }}
+        transition={{ repeat: Infinity, duration: 2 }}
+      />
+      <motion.div
+        className="absolute bottom-10 left-1/2 bg-purple-500 w-12 h-12 rounded-full"
+        animate={{ y: [0, -10, 0] }}
+        transition={{ repeat: Infinity, duration: 4 }}
+      />
+    </div>
+  );
+}
