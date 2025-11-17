755fcde7b1926e9cd105d4a5504fc0bcccc9b6c5
Aniket Sakharkar <aniketrita2612@gmail.com>
2025-11-16 22:31:55 -0500

    Updated MouseTrail.tsx

--- src/components/MouseTrail.tsx
+++ src/components/MouseTrail.tsx
@@ -4,19 +4,6 @@ export default function MouseTrail() {
   useEffect(() => {
     const dot = document.createElement("div");
     dot.id = "mouse-trail-dot";
-    Object.assign(dot.style, {
-      position: "fixed",
-      left: "0px",
-      top: "0px",
-      width: "12px",
-      height: "12px",
-      background: "#3b82f6",
-      borderRadius: "50%",
-      pointerEvents: "none",
-      zIndex: "99999",
-      transform: "translate(-50%, -50%)",
-      transition: "transform 0.08s ease-out",
-    });
     document.body.appendChild(dot);
 
     const moveDot = (e: MouseEvent) => {
