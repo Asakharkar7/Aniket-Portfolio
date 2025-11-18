b59d68d45d3ff87d9bf7973f7d133cc543c8d403
Aniket Sakharkar <aniketrita2612@gmail.com>
2025-11-17 23:01:26 -0500

    Updated Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -10,6 +10,7 @@ import {
 import { useMemo, useEffect } from "react";
 import { useTypewriter } from "../hooks/useTypewriter";
 import GameHubMenu from "./GameHub/GameHubMenu.tsx";
+import DeskScene from "./DeskScene.tsx"
 
 export default function Hero() {
   const roles = useMemo(
@@ -125,7 +126,12 @@ export default function Hero() {
               <ArrowDown size={32} />
             </a>
           </div>
-
+          
+          {/* Desk Scene Model */}
+          <div className="mt-20 flex justify-center">
+            <DeskScene />
+          </div>
+          
           {/* 🎮 GameHub Menu */}
           <div className="mt-20">
             <GameHubMenu />
