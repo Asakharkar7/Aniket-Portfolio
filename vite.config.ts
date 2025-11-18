dc5007877092d8b75f87a2efc2de4b0e5b8a3546
Aniket Sakharkar <aniketrita2612@gmail.com>
2025-11-18 18:37:41 -0500

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  // ✅ Set base to your repo name for project site
   base: '/Aniket-Portfolio/',
   optimizeDeps: {
     exclude: ['lucide-react'],
